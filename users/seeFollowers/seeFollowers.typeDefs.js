94702a2b66d47e9019296eae21a686a0403bc485
lezendev <lezendev@gmail.com>
2021-12-07 05:29:51 +0900

    4th

---
+++ users/seeFollowers/seeFollowers.typeDefs.js
@@ -0,0 +1,16 @@
+import {gql} from "apollo-server";
+
+export default gql`
+    type SeeFollowersResult {
+        ok: Boolean!
+        error: String
+        followers: [User]
+        totalPages: Int
+        
+    }
+    
+    type Query {
+        seeFollowers(username:String!, page:Int!) : SeeFollowersResult!
+    }
+    
+`;
